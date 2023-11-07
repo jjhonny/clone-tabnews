2f20af0c4eda62f24d97eb7a19fd4e0e1e28ab09
Jhonny Dias <124160277+jjhonny@users.noreply.github.com>
2023-11-07 13:58:48 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Assistindo o curso.dev do Filipe Deschamps!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
